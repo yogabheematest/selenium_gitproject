e2810498a9cfe3f6a80e0d90b5c12b62cab21b9d
yoganandbheema <yogabheematest@gmail.com>
2022-09-28 19:21:42 +0530

    first commits

---
+++ src/main/java/myproject/gitsample_project/App.java
@@ -0,0 +1,13 @@
+package myproject.gitsample_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
